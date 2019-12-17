4b3376ad1ec8baa9aa25e0f5cfc8203ccf17c79b
okkursevi10 <okkursevi10@UC5PC6>
2019-12-17 19:39:57 +0100

    doGet i doPost metode

--- TuristickaAgencija/src/controller/RegistracioniServlet.java
+++ TuristickaAgencija/src/controller/RegistracioniServlet.java
@@ -26,16 +26,33 @@ public class RegistracioniServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		
+		//preuzimanje parametara sa forme tj. iz url
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		String repeatedPassword = request.getParameter("repeatedPassword");
+		
+		
+		System.out.println(userName);
+		System.out.println(password);
+		System.out.println(repeatedPassword);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		String repeatedPassword = request.getParameter("repeatedPassword");
+		
+		
+		System.out.println(userName);
+		System.out.println(password);
+		System.out.println(repeatedPassword);
 	}
+	
 
 }
